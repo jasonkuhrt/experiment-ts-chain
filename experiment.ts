c7c311f0775b70bb8796647706d8f1b7b613d36d
Jason Kuhrt <jasonkuhrt@me.com>
2024-11-28 10:16:58 -0500

    init

---
+++ experiment.ts
@@ -0,0 +1,292 @@
+
+interface T {
+  name: string
+}
+
+interface Context {
+  transports: { [_:string]: T }
+  current: null | string
+}
+
+interface ContextEmpty extends Context {
+  transports: {}
+  current: null
+}
+
+// Approach 1: mapped type + conditional type
+// interface Chain<
+//   out $C extends Context,
+// > {
+//   _: $C
+//   set:  <
+//     const name extends $C['transports'][keyof $C['transports']]['name'],
+//   > (name: name) => Chain<{
+//     [_ in keyof $C]:
+//       _ extends 'current'
+//         ? name
+//         : $C[_]
+//     }>
+//   addAndSetIfFirst: <
+//     t extends T,
+//   > (t: t) => Chain<{
+//       [_ in keyof $C]:
+//         _ extends 'current'    ? ($C['current'] extends string ? $C['current'] : t['name']) :
+//         _ extends 'transports' ? { [_ in t['name']]: t } & $C['transports'] :
+//         $C[_]
+//     }>
+// }
+
+// Approach 2: explicit recreate
+// interface Chain<
+//   out $C extends Context,
+// > {
+//   _: $C
+//   set: <
+//     const name extends $C['transports'][keyof $C['transports']]['name'],
+//   > (name: name) => Chain<{
+//       current: name,
+//       transports: $C['transports'],
+//     }>
+//   addAndSetIfFirst: <
+//     t extends T,
+//   > (t: t) => Chain<{
+//       current:    ($C['current'] extends string ? $C['current'] : t['name'])
+//       transports: { [_ in t['name']]: t } & $C['transports']
+//     }>
+// }
+
+// Approach 3: explicit recreate + type parameter
+interface Chain<
+  out $C extends Context,
+> {
+  _: $C
+  set: <
+    const name extends $C['transports'][keyof $C['transports']]['name'],
+    c extends Context = {
+      current: name,
+      transports: $C['transports'],
+    }
+  > (name: name) => Chain<c>
+  addAndSetIfFirst: <
+    t extends T,
+    c extends Context = {
+      current:    ($C['current'] extends string ? $C['current'] : t['name'])
+      transports: { [_ in t['name']]: t } & $C['transports']
+    }
+  > (t: t) => Chain<c>
+}
+
+declare const t1: { name: 't1' }
+declare const t2: { name: 't2' }
+declare const t3: { name: 't3' }
+declare const t4: { name: 't4' }
+declare const t5: { name: 't5' }
+declare const t6: { name: 't6' }
+declare const t7: { name: 't7' }
+declare const t8: { name: 't8' }
+declare const t9: { name: 't9' }
+declare const t10: { name: 't10' }
+declare const t11: { name: 't11' }
+declare const t12: { name: 't12' }
+declare const t13: { name: 't13' }
+declare const t14: { name: 't14' }
+declare const t15: { name: 't15' }
+declare const t16: { name: 't16' }
+declare const t17: { name: 't17' }
+declare const t18: { name: 't18' }
+declare const t19: { name: 't19' }
+declare const t20: { name: 't20' }
+declare const t21: { name: 't21' }
+declare const t22: { name: 't22' }
+declare const t23: { name: 't23' }
+declare const t24: { name: 't24' }
+declare const t25: { name: 't25' }
+declare const t26: { name: 't26' }
+declare const t27: { name: 't27' }
+declare const t28: { name: 't28' }
+declare const t29: { name: 't29' }
+declare const t30: { name: 't30' }
+declare const t31: { name: 't31' }
+declare const t32: { name: 't32' }
+declare const t33: { name: 't33' }
+declare const t34: { name: 't34' }
+declare const t35: { name: 't35' }
+declare const t36: { name: 't36' }
+declare const t37: { name: 't37' }
+declare const t38: { name: 't38' }
+declare const t39: { name: 't39' }
+declare const t40: { name: 't40' }
+declare const t41: { name: 't41' }
+declare const t42: { name: 't42' }
+declare const t43: { name: 't43' }
+declare const t44: { name: 't44' }
+declare const t45: { name: 't45' }
+declare const t46: { name: 't46' }
+declare const t47: { name: 't47' }
+declare const t48: { name: 't48' }
+declare const t49: { name: 't49' }
+declare const t50: { name: 't50' }
+declare const t51: { name: 't51' }
+declare const t52: { name: 't52' }
+declare const t53: { name: 't53' }
+declare const t54: { name: 't54' }
+declare const t55: { name: 't55' }
+declare const t56: { name: 't56' }
+declare const t57: { name: 't57' }
+declare const t58: { name: 't58' }
+declare const t59: { name: 't59' }
+declare const t60: { name: 't60' }
+declare const t61: { name: 't61' }
+declare const t62: { name: 't62' }
+declare const t63: { name: 't63' }
+declare const t64: { name: 't64' }
+declare const t65: { name: 't65' }
+declare const t66: { name: 't66' }
+declare const t67: { name: 't67' }
+declare const t68: { name: 't68' }
+declare const t69: { name: 't69' }
+declare const t70: { name: 't70' }
+declare const t71: { name: 't71' }
+declare const t72: { name: 't72' }
+declare const t73: { name: 't73' }
+declare const t74: { name: 't74' }
+declare const t75: { name: 't75' }
+declare const t76: { name: 't76' }
+declare const t77: { name: 't77' }
+declare const t78: { name: 't78' }
+declare const t79: { name: 't79' }
+declare const t80: { name: 't80' }
+declare const t81: { name: 't81' }
+declare const t82: { name: 't82' }
+declare const t83: { name: 't83' }
+declare const t84: { name: 't84' }
+declare const t85: { name: 't85' }
+declare const t86: { name: 't86' }
+declare const t87: { name: 't87' }
+declare const t88: { name: 't88' }
+declare const t89: { name: 't89' }
+declare const t90: { name: 't90' }
+declare const t91: { name: 't91' }
+declare const t92: { name: 't92' }
+declare const t93: { name: 't93' }
+declare const t94: { name: 't94' }
+declare const t95: { name: 't95' }
+declare const t96: { name: 't96' }
+declare const t97: { name: 't97' }
+declare const t98: { name: 't98' }
+declare const t99: { name: 't99' }
+declare const t100: { name: 't100' }
+
+declare const chain: Chain<ContextEmpty>
+const c = chain
+  .addAndSetIfFirst(t1)
+  .set('t1')
+  .addAndSetIfFirst(t2)
+  .addAndSetIfFirst(t3)
+  .set('t3')
+  .addAndSetIfFirst(t4)
+  .addAndSetIfFirst(t5)
+  .addAndSetIfFirst(t6)
+  .addAndSetIfFirst(t7)
+  .addAndSetIfFirst(t8)
+  .addAndSetIfFirst(t9)
+  .addAndSetIfFirst(t10)
+  .addAndSetIfFirst(t11)
+  .addAndSetIfFirst(t12)
+  .addAndSetIfFirst(t13)
+  .addAndSetIfFirst(t14)
+  .addAndSetIfFirst(t15)
+  .addAndSetIfFirst(t16)
+  .addAndSetIfFirst(t17)
+  .addAndSetIfFirst(t18)
+  .addAndSetIfFirst(t19)
+  .addAndSetIfFirst(t20)
+  .addAndSetIfFirst(t21)
+  .addAndSetIfFirst(t22)
+  .addAndSetIfFirst(t23)
+  .addAndSetIfFirst(t24)
+  .addAndSetIfFirst(t25)
+  .addAndSetIfFirst(t26)
+  .addAndSetIfFirst(t27)
+  .addAndSetIfFirst(t28)
+  .addAndSetIfFirst(t29)
+  .addAndSetIfFirst(t30)
+  .addAndSetIfFirst(t31)
+  .set('t18')
+  .addAndSetIfFirst(t32)
+  .addAndSetIfFirst(t33)
+  .addAndSetIfFirst(t34)
+  .addAndSetIfFirst(t35)
+  .addAndSetIfFirst(t36)
+  .addAndSetIfFirst(t37)
+  .addAndSetIfFirst(t38)
+  .addAndSetIfFirst(t39)
+  .addAndSetIfFirst(t40)
+  .addAndSetIfFirst(t41)
+  .addAndSetIfFirst(t42)
+  .addAndSetIfFirst(t43)
+  .addAndSetIfFirst(t44)
+  .addAndSetIfFirst(t45)
+  .addAndSetIfFirst(t46)
+  .addAndSetIfFirst(t47)
+  .addAndSetIfFirst(t48)
+  .addAndSetIfFirst(t49)
+  .addAndSetIfFirst(t50)
+  .addAndSetIfFirst(t51)
+  .addAndSetIfFirst(t52)
+  .addAndSetIfFirst(t53)
+  .addAndSetIfFirst(t54)
+  .addAndSetIfFirst(t55)
+  .addAndSetIfFirst(t56)
+  .addAndSetIfFirst(t57)
+  .addAndSetIfFirst(t58)
+  .addAndSetIfFirst(t59)
+  .addAndSetIfFirst(t60)
+  .addAndSetIfFirst(t61)
+  .addAndSetIfFirst(t62)
+  .addAndSetIfFirst(t63) // <-- fails here with approach "mapped type + conditional type"
+  //--------------------
+  .addAndSetIfFirst(t64)
+  .addAndSetIfFirst(t65)
+  .addAndSetIfFirst(t66)
+  .addAndSetIfFirst(t67)
+  .addAndSetIfFirst(t68)
+  .addAndSetIfFirst(t69)
+  .addAndSetIfFirst(t70)
+  .addAndSetIfFirst(t71)
+  .addAndSetIfFirst(t72)
+  .addAndSetIfFirst(t73)
+  .addAndSetIfFirst(t74)
+  .addAndSetIfFirst(t75)
+  .addAndSetIfFirst(t76)
+  .addAndSetIfFirst(t77)
+  .addAndSetIfFirst(t78)
+  .addAndSetIfFirst(t79) // <-- fails here with approach "explicit recreate"
+  //--------------------
+  .addAndSetIfFirst(t80)
+  .addAndSetIfFirst(t81)
+  .addAndSetIfFirst(t82)
+  .addAndSetIfFirst(t83)
+  .addAndSetIfFirst(t84)
+  .addAndSetIfFirst(t85)
+  .addAndSetIfFirst(t86)
+  .addAndSetIfFirst(t87)
+  .addAndSetIfFirst(t88)
+  .addAndSetIfFirst(t89)
+  .addAndSetIfFirst(t90)
+  .addAndSetIfFirst(t91)
+  .addAndSetIfFirst(t92)
+  .addAndSetIfFirst(t93)
+  .addAndSetIfFirst(t94)
+  .addAndSetIfFirst(t95)
+  // .addAndSetIfFirst(t96) // <-- fails here with approach "explicit recreate + type parameter"
+  //-------------------- 
+  // .addAndSetIfFirst(t97)
+  // .addAndSetIfFirst(t98)
+  // .addAndSetIfFirst(t99)
+  // .addAndSetIfFirst(t100)
+  .set('t38')
+  ._
+
+type c = typeof c
+c.current
